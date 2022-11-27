4d7f7dd52dee7c8ed0e9a0545ef1dd2e07d0c5b3
israniraksha <israni.r@northeastern.edu>
2022-11-27 18:09:54 -0500

    Added packages and first set of classes

---
+++ INFO5100-PetInsurance/src/info5100/petinsurance/model/Address.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package info5100.petinsurance.model;
+
+/**
+ *
+ * @author rakshaisrani
+ */
+public class Address {
+    
+    int id;
+    String addressLine1;
+    String addressLine2;
+    String city;
+    String state;
+    String country;
+    int zipCode;
+    
+}
